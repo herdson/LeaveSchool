e84c0bc4b93c5ebdcd193f5b912b001fb2985b4a
HO GYEONG CHOI <devherdson@gmail.com>
2017-11-09 23:49:12 +0900

    Obstacle detector has been made

--- proejct4_assignment.py
+++ proejct4_assignment.py
@@ -62,8 +62,8 @@ lspeed2 = 15
 rspeed2 = 8
 
 # used avoid_obstacle
-obstacle_left_speed = 30
-obstacle_right_speed = 20
+obstacle_left_speed = 10
+obstacle_right_speed = 50
 
 # used isobstacle 
 isobstacle_left_speed = 15
@@ -75,47 +75,40 @@ linefound_right_speed = 8
 
 # Distance detector
 dis = 15
-isPosition = False
-isTurn = False
+isPosition = 0
 
 # =======================================================================
 # Define LineTracing Function
 # =======================================================================
 def obstacle_linetracing(distance_value):
-    detailTurn(obstacle_left_speed, obstacle_right_speed, 2) #First Right Turn
-    while distance_value < dis: # is obstacle
-        detailTurn(isobstacle_left_speed, isobstacle_right_speed, 2)
-    # not obstacle 
+    detailTurn(obstacle_left_speed, obstacle_right_speed, 1) #First Right Turn
+    print("Enter 2")
+    while (getDistance() < dis): # is obstacle 
+        detailTurn(isobstacle_left_speed, isobstacle_right_speed, 1)
+        print("Enter 1")
+    detailTurn(obstacle_right_speed, obstacle_left_speed, 0.85) #First Left Turn
+    print("Enter 3") # not obstacle 
     try:
-        go_forward(20, 2) # constance
+        go_forward(30, 2) # constance
     except Exception:
         print("Detected Error")
     else:
         print("No Problem")
     finally:
-        detailTurn(obstacle_right_speed, obstacle_left_speed, 2) #Last Left Turn
+        detailTurn(obstacle_right_speed, obstacle_left_speed, 1) # Second Left Turn
+        detailTurn(obstacle_left_speed, obstacle_right_speed, 0.85) # Second Right Turn
         while getLeftmostled() or getLeftlessled(): # line found
             go_forward(10, 2) # constance
-        while getCenterled() or getRightlessled(): # Stabilize after Turn
+        while getCenterled() or getRightlessled(): # Line Sorting
             detailTurn(linefound_left_speed, linefound_right_speed, 2)
     print("obstacle avoid Success")
 
 def goLineTracing(lspeed1, rspeed1, lspeed2, rspeed2):
-    global isTurn
-    if not(getRightmostled()) or getLeftmostled(): # Turn Section
-        isTurn = True
+    if not(getLeftlessled()) or not(getLeftmostled()):
         go_forward_fine(lspeed1, rspeed1)
-    elif getRightlessled() :# Need a Stabilization
+    elif not(getRightlessled()) or not(getRightmostled()):
         go_forward_fine(lspeed2, rspeed2)
-    elif getLeftlessled(): # Need a Stabilization
-        go_forward_fine(rspeed2, lspeed2)
     else:
-        if isTurn: # Stabilize after Turn
-            while not(getRightlessled()) and not(getLeftlessled()):
-                go_forward_fine((lspeed1 - 5), (rspeed2 - 5))
-            print("Turn Success")
-            isTurn = False 
-        # only forward
         go_forward_any(speed)
 
 def AllWhitespace():
@@ -143,26 +136,25 @@ def AllWhitespace():
 try:
     while True:
         # Checking line
-        print("leftmostled  detects black line(0) or white ground(1): " + str(getLeftmostled()))
-        print("leftlessled  detects black line(0) or white ground(1): " + str(getLeftlessled()))
-        print("centerled    detects black line(0) or white ground(1): " + str(getCenterled()))
-        print("rightlessled detects black line(0) or white ground(1): " + str(getRightlessled()))
-        print("rightmostled detects black line(0) or white ground(1): " + str(getRightmostled()))
-
-        # Line checking latency time.
-        sleep(1)
+        # print("leftmostled  detects black line(0) or white ground(1): " + str(getLeftmostled()))
+        # print("leftlessled  detects black line(0) or white ground(1): " + str(getLeftlessled()))
+        # print("centerled    detects black line(0) or white ground(1): " + str(getCenterled()))
+        # print("rightlessled detects black line(0) or white ground(1): " + str(getRightlessled()))
+        # print("rightmostled detects black line(0) or white ground(1): " + str(getRightmostled()))
 
         # ultra sensor replies the distance back
         distance = getDistance()
-        goLineTracing(lspeed1,rspeed1, lspeed2, rspeed2)
+        #goLineTracing(lspeed1,rspeed1, lspeed2, rspeed2)
 
+        print(distance)
+        
         if distance < dis: #obstacle found
             stop()
             obstacle_linetracing(distance)
 
-        if getLeftmostled() and getCenterled() and getRightmostled(): #reading sensor - All White
-            stop()
-            AllWhitespace()
+        #if getLeftmostled() and getCenterled() and getRightmostled(): #reading sensor - All White
+        #    stop()
+        #    AllWhitespace()
 
     print ("Shutdown")
     pwm_low()
